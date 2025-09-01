f37716e0f4cd7756b2e39c60f536945ddab41b1e
k-Iwabuchi55 <kaishi-iwabuchi@liberal.tokyo>
2025-09-01 12:19:54 +0900

    feat: Google Analytics4 実装

--- dsinage-lp-blue/app/layout.tsx
+++ dsinage-lp-blue/app/layout.tsx
@@ -3,12 +3,21 @@ import { Inter, Noto_Sans_JP } from 'next/font/google'
 import './globals.css'
 import { Analytics } from '@vercel/analytics/next';
 
+import { 
+  GoogleAnalytics, 
+  // GoogleTagManager 実装予定
+ } from '@next/third-parties/google'
+
+
 const inter = Inter({ subsets: ['latin'] })
 const notoSansJP = Noto_Sans_JP({ 
   subsets: ['latin'],
   weight: ['300', '400', '500', '700', '900'],
   variable: '--font-noto-sans-jp',
 })
+const isProduction = process.env.NODE_ENV === 'production'
+const gaId = process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID!
+// const gtmId = process.env.NEXT_PUBLIC_GOOGLE_TAG_MANAGER_ID!
 
 export const metadata: Metadata = {
   title: 'D-signage | デジタルサイネージで新しい広告体験を',
@@ -28,9 +37,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="ja">
+      {/* {isProduction && <GoogleTagManager gtmId={gtmId} />} */}
       <body className={`${inter.className} ${notoSansJP.variable}`}>
         {children}
         <Analytics />
+        {isProduction && <GoogleAnalytics gaId={gaId} />}
+        
       </body>
     </html>
   )
